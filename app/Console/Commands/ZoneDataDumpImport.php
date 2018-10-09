9a5351607289c821bd31005e821db811de9afffd
Akkadius <akkadius1@gmail.com>
2018-10-09 16:40:58 -0500

    Implement command import type "all"

--- app/Console/Commands/ZoneDataDumpImport.php
+++ app/Console/Commands/ZoneDataDumpImport.php
@@ -22,6 +22,7 @@ class ZoneDataDumpImport extends Command
         {zone_short_name}
         {zone_instance_version}
         {dump_type : npc|door|object|groundspawn|zonepoint|zone|all}
+        {--s|skip-confirmation : Skips file prompt confirmation}
     ';
 
     /**
@@ -57,6 +58,7 @@ class ZoneDataDumpImport extends Command
         $zone_short_name       = $this->argument('zone_short_name');
         $zone_instance_version = $this->argument('zone_instance_version');
         $dump_type             = $this->argument('dump_type');
+        $skip_confirmation     = $this->option('skip-confirmation');
 
         /**
          * One-off type transform to match file name
@@ -64,6 +66,7 @@ class ZoneDataDumpImport extends Command
         if ($dump_type == "groundspawn") {
             $dump_type = "grounditem";
         }
+
         if ($dump_type == "object") {
             $dump_type = "objects";
         }
@@ -75,57 +78,78 @@ class ZoneDataDumpImport extends Command
         $file_to_read   = "";
         $file_timestamp = "";
 
-        foreach ($files as $file) {
-            $file                      = strtolower($file);
-            $file_clean                = str_replace(".csv", "", $file);
-            $file_parameters           = explode("_", $file_clean);
-            $zone_short_name_parameter = array_get($file_parameters, 0, '');
-            $dump_type_parameter       = array_get($file_parameters, 1, '');
-            $time_stamp_parameter      = array_get($file_parameters, 2, '');
+        $imports_to_run = [$dump_type];
 
-            if ($zone_short_name == $zone_short_name_parameter && $dump_type == $dump_type_parameter) {
-                if ($this->confirm("Use this file? {$file}")) {
-                    $file_to_read   = $file;
-                    $file_timestamp = $time_stamp_parameter;
-                    break;
+        if ($dump_type == "all") {
+            $imports_to_run = [
+                "npc",
+                "door",
+                "grounditem",
+                "objects",
+                "zonepoint"
+            ];
+        }
+
+        foreach ($imports_to_run as $import_type) {
+
+            foreach ($files as $file) {
+                $file                      = strtolower($file);
+                $file_clean                = str_replace(".csv", "", $file);
+                $file_parameters           = explode("_", $file_clean);
+                $zone_short_name_parameter = array_get($file_parameters, 0, '');
+                $dump_type_parameter       = array_get($file_parameters, 1, '');
+                $time_stamp_parameter      = array_get($file_parameters, 2, '');
+
+                if ($zone_short_name == $zone_short_name_parameter && $import_type == $dump_type_parameter) {
+                    if ($skip_confirmation) {
+                        $file_to_read   = $file;
+                        $file_timestamp = $time_stamp_parameter;
+                        break;
+                    }
+
+                    if ($this->confirm("Use this file? {$file}")) {
+                        $file_to_read   = $file;
+                        $file_timestamp = $time_stamp_parameter;
+                        break;
+                    }
                 }
             }
-        }
 
-        if (empty($file_to_read)) {
-            $this->error("No file to read!");
-            exit;
-        }
+            if (empty($file_to_read)) {
+                $this->error("No file to read!");
+                exit;
+            }
 
-        $this->info("Reading file '{$file}' time: {$file_timestamp}");
+            $this->info("Reading file '{$file}' time: {$file_timestamp}");
 
-        /**
-         * Process import
-         */
-        $zone_data_dump_import_service
-            ->setFile($file)
-            ->setZoneShortName($zone_short_name)
-            ->setZoneInstanceVersion($zone_instance_version);
+            /**
+             * Process import
+             */
+            $zone_data_dump_import_service
+                ->setFile($file)
+                ->setZoneShortName($zone_short_name)
+                ->setZoneInstanceVersion($zone_instance_version);
 
-        switch ($dump_type) {
-            case "npc":
-                $zone_data_dump_import_service->importNpcData();
-                break;
-            case "door":
-                $zone_data_dump_import_service->importDoorData();
-                break;
-            case "grounditem":
-                $zone_data_dump_import_service->importGroundSpawnData();
-                break;
-            case "objects":
-                $zone_data_dump_import_service->importObjectData();
-                break;
-            case "zonepoint":
-                $zone_data_dump_import_service->importZonePointData();
-                break;
-            case "all":
-                $zone_data_dump_import_service->importAll();
-                break;
+            switch ($import_type) {
+                case "npc":
+                    $zone_data_dump_import_service->importNpcData();
+                    break;
+                case "door":
+                    $zone_data_dump_import_service->importDoorData();
+                    break;
+                case "grounditem":
+                    $zone_data_dump_import_service->importGroundSpawnData();
+                    break;
+                case "objects":
+                    $zone_data_dump_import_service->importObjectData();
+                    break;
+                case "zonepoint":
+                    $zone_data_dump_import_service->importZonePointData();
+                    break;
+                case "all":
+                    $zone_data_dump_import_service->importAll();
+                    break;
+            }
         }
 
         foreach ($zone_data_dump_import_service->getCreated() as $created_type => $count) {
